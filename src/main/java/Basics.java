6ae8c6005fcebc687041584dd5f7ea108428dd17
zhenzeyang <zhenze.yang@mail.utoronto.ca>
2022-10-09 15:50:35 -0400

    quiz1

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -160,12 +160,11 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-        if (arr.length < 1) {
-            return current_sum;
-        }
+        if (arr.length > 1) {
             for (int i = 1; i <= arr.length; i += 2) {
                 current_sum = current_sum + arr[i];
             }
+        }
         return current_sum;
     }
     /*
